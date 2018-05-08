e67ca7512de6bf16e824a60ef19678b4714ccb4c
Bishal Shrestha <bishalshrestha33@gmail.com>
2018-05-08 23:36:36 +0545

    Add method for indexing site

--- app/api/v1/parsed_content_controller.rb
+++ app/api/v1/parsed_content_controller.rb
@@ -1,6 +1,37 @@
+require 'open-uri'
+
 module Api
   module V1
-    class ParsedContentController < ApplicationController
+    class ParsedContentController < JSONAPI::ResourceController
+      def create
+        params["data"]["attributes"]["data"] = index_site
+        super
+      end
+
+      private
+
+      def index_site
+        tries = 3
+        begin
+          doc = Timeout::timeout(25) do
+            Nokogiri::HTML(open(params["data"]["attributes"]["url"]))
+          end
+        rescue StandardError => e
+          puts "This link has error: "
+          raise e
+        end
+        links = doc.xpath('//a').map {|element| element["href"]}.compact
+        h1s = doc.xpath('//h1').map {|a| a.text.squish}
+        h2s = doc.xpath('//h2').map {|a| a.text.squish}
+        h3s = doc.xpath('//h3').map {|a| a.text.squish}
+
+        {
+          "links": links,
+          "h1": h1s,
+          "h2": h2s,
+          "h3": h3s,
+        }
+      end
     end
   end
 end
